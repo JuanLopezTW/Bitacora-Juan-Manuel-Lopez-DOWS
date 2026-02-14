ab90532300048afbcd9d38b2d28b2139c4af5bbf
Juan Lopez <juan.lbarrera@mail.escuelaing.edu.co>
2026-02-13 21:16:58 -0500

    estructura inicial

---
+++ DOSW-Bitacora-JuanLopez/src/main/java/edu/dosw/bitacora/DOSW_Bitacora_JuanLopez/DoswBitacoraJuanLopezApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.bitacora.DOSW_Bitacora_JuanLopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswBitacoraJuanLopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswBitacoraJuanLopezApplication.class, args);
+	}
+
+}
---
+++ DOSW-Bitacora-JuanLopez/src/test/java/edu/dosw/bitacora/DOSW_Bitacora_JuanLopez/DoswBitacoraJuanLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.bitacora.DOSW_Bitacora_JuanLopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswBitacoraJuanLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
